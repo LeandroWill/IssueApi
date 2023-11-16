5c6dad24d7ab1e8353cdeb93b6de20dae07a47de
LeandroWill <leandro_willgodoi@hotmail.com>
2023-11-16 15:02:17 -0300

    Primeiro commit
    Projeto funcionando

---
+++ TodoApi/TodoApi/Data/IssueDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+namespace TodoApi.Data {
+    public class IssueDbContext : DbContext {
+        public IssueDbContext(DbContextOptions<IssueDbContext> options) : base(options) {
+        }
+
+        public DbSet<Issue> Issues { get; set; }
+    }
+}
