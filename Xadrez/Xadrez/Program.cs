6ae8c87adf050892aa2c0d5f76ba8a62f013d51d
LeandroWill <leandro_willgodoi@hotmail.com>
2023-12-06 10:07:28 -0300

    Metodo de imprimir tabuleiro
    Começando método de imprimir tabuleiro

--- Xadrez/Xadrez/Program.cs
+++ Xadrez/Xadrez/Program.cs
@@ -1,6 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using tabuleiro;
+using Xadrez;
 
 Console.WriteLine("Hello, World!");
 
 Tabuleiro tab = new Tabuleiro(8, 8);
+
+Tela.imprimirTabuleiro(tab);
+
+Console.ReadLine();
